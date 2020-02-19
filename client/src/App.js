fd2ffc9276452cd10fcd5a09e2e390abcc5ddec9
ohmaga <ohmaga@naver.com>
2020-02-19 17:05:01 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -12,41 +12,30 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto"
   },
   table: {
     minWidth: 1080
   }
-})
-const customers = [
-  {
-    'id': 1,
-    'img': 'http://placeimg.com/64/64/1',
-    'name': '홍길동',
-    'birthday': '961120',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': 2,
-    'img': 'http://placeimg.com/64/64/2',
-    'name': '홍길순',
-    'birthday': '920205',
-    'gender': '여자',
-    'job': '간호사'
-  },
-  {
-    'id': 3,
-    'img': 'http://placeimg.com/64/64/3',
-    'name': '홍길자',
-    'birthday': '931005',
-    'gender': '여자',
-    'job': '회사원'
-  }
-]
+});
 
 class App extends Component {
+  state = {
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({customers:res}))
+    .catch(err=>console.log(err));
+  }
+
+  callApi = async ()=>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
   render(){
     const { classes } = this.props;
     return (
@@ -63,26 +52,25 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-        {
-          customers.map(c=>{
-            return(
-              <Customer
-                key={c.id} /* map 을 사용할 경우 꼭 있어야 하는 props 이라서 넣었음 */
-                id={c.id}
-                image={c.img}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            )
-          })
-        }
+            {
+              this.state.customers ? this.state.customers.map(c=>{
+                return(
+                  <Customer
+                    key={c.id} /* map 을 사용할 경우 꼭 있어야 하는 props 이라서 넣었음 */
+                    id={c.id}
+                    image={c.img}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                )
+              }) : ""
+            }
           </TableBody>
         </Table>
       </Paper>
     );
   }
 }
-
 export default withStyles(styles)(App);
